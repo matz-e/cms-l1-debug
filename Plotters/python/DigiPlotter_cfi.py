763ca1daaf13d51efe46c608851c3cf341ccaa82
Matthias Wolf <matthias@sushinara.net>
2012-10-03 09:24:36 -0500

    Add python config for DigiPlotter.

--- Plotters/python/DigiPlotter_cfi.py
+++ Plotters/python/DigiPlotter_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 digiPlotter = cms.EDAnalyzer('DigiPlotter',
-        ecalDigis = cms.InputTag('ecalDigis'),
+        ecalDigis = cms.InputTag('ecalDigis', 'ebDigis'),
         hcalDigis = cms.InputTag('hcalDigis'))
