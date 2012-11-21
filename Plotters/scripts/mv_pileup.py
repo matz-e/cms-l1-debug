73a8462cd73e36d54980686a37176080a195d4b0
Matthias Wolf <matthias@sushinara.net>
2012-11-20 20:00:14 -0600

    Add script to move calculated data pileup distribution.
    Allows to plot data pileup distribution with MC ones.

---
+++ Plotters/scripts/mv_pileup.py
@@ -0,0 +1,18 @@
+import ROOT as r
+import sys
+
+r.gROOT.SetBatch()
+
+if len(sys.argv) != 3:
+    sys.stderr.write('usage: {p} source dest\n'.format(p=sys.argv[0]))
+    sys.exit(1)
+
+infile = r.TFile(sys.argv[1])
+hist = infile.Get('pileup')
+hist.Scale(604031.0 / hist.Integral())
+
+outfile = r.TFile(sys.argv[2], 'CREATE')
+dir = outfile.mkdir('pileUpPlotter')
+dir.cd()
+hist.Write()
+outfile.Close()
