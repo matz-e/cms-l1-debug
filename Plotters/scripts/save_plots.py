4ccdf2d70c6149482a2381e11d19e07e9bd4a4aa
Matthias Wolf <matthias@sushinara.net>
2013-02-28 08:07:14 -0600

    Rework plotting script.  This should be a lot smoother now, with better plotting limits and removal of THStacks.

--- Plotters/scripts/save_plots.py
+++ Plotters/scripts/save_plots.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-mc_cmp = False
+plot_only = ""
 
 # Argument parsing
 # vvv
@@ -8,15 +8,8 @@ mc_cmp = False
 import re
 import sys
 
-if len(sys.argv) > 1 and sys.argv[1].endswith('.py'):
-    sys.argv.pop(0)
-if len(sys.argv) == 2 and ':' in sys.argv[1]:
-    argv = sys.argv[1].split(':')
-else:
-    argv = sys.argv[1:]
-
 new_args = []
-for arg in argv:
+for arg in sys.argv[1:]:
     if '=' not in arg:
         new_args.append(arg)
         continue
@@ -29,6 +22,8 @@ for arg in argv:
         globals()[k] = v.lower() in ('y', 'yes', 'true', 't', '1')
     else:
         globals()[k] = type(globals()[k])(v)
+
+sys.argv = [sys.argv[0]] + new_args
 
 import math
 import os
@@ -43,7 +38,23 @@ r.gStyle.SetTitleBorderSize(0)
 r.gStyle.SetOptStat(0)
 # r.gStyle.SetTitleAlign(22)
 
-override_limits = { 'ecal_en_tot': (0, 750) }
+class CustomLimits:
+    def __init__(self, map):
+        self.__dict = map
+    def __contains__(self, key):
+        if self.__getitem__(key):
+            return True
+        return False
+    def __getitem__(self, key):
+        for (k, v) in self.__dict.items():
+            if k.search(key):
+                return v
+        return None
+
+override_limits = CustomLimits({
+        re.compile('ecal_en_tot'): (0, 750),
+        re.compile('cal_time'): (-100, 100)
+        })
 
 class Plots:
     def __init__(self, title='', legend=None, logplot=False,
@@ -58,11 +69,28 @@ class Plots:
         self.__plots = []
         self.__title = title
 
+    def find_x_limits(self):
+        if not self.__adjust:
+            return self.__limits
+
+        self.__limits = [float("inf"), float("-inf")]
+
+        frac = .0 if self.__logplot else .05
+
+        for plot in self.__plots:
+            xmax = self.find_limit(plot, frac=frac)
+            xmin = self.find_limit(plot, False, frac=frac)
+            if self.__limits:
+                self.__limits = (
+                        min(self.__limits[0], xmin),
+                        max(self.__limits[1], xmax))
+        return self.__limits
+
     def add(self, plot, legend_title):
         self.__plots.append(plot)
 
         style = "l"
-        if isinstance(plot, r.TProfile) or "data" in legend_title.lower():
+        if isinstance(plot, r.TProfile) or "p" in plot.GetOption().lower():
             style = "p"
         self.__legend.AddEntry(plot, legend_title, style) 
 
@@ -76,16 +104,18 @@ class Plots:
             else:
                 self.__limits = (xmin, xmax)
 
-    def find_limit(self, plot, upper=True):
+    def find_limit(self, plot, upper=True, frac=0):
         nbins = plot.GetNbinsX()
         bins = [plot.GetBinContent(i) for i in range(1, nbins)]
+        # pmax = plot.GetMaximum()
+        pmin = frac * max([p.GetBinContent(p.GetMaximumBin()) for p in self.__plots])
 
         index = []
         for (i, bin) in enumerate(bins[:-1]):
-            index.append(bin > 0 and bins[i + 1] > 0)
+            index.append(bin > pmin and bins[i + 1] > pmin)
 
         if True not in index:
-            index = map(lambda b: b > 0, bins)
+            index = map(lambda b: b > pmin, bins)
         if True not in index:
             return 0
 
@@ -95,9 +125,44 @@ class Plots:
             bound = plot.GetBinLowEdge(index.index(True) + 1)
         return bound
 
+    def get_max(self, plt):
+        return self.get_ex(plt, max, float.__gt__, 1.5, "-inf")
+
+    def get_min(self, plt):
+        return self.get_ex(plt, min, float.__lt__, .75, "inf")
+
+    def get_ex(self, plt, cmp, op, f, default):
+        contents = filter(lambda x: x > 0,
+                [plt.GetBinContent(i + 1) for i in range(plt.GetNbinsX())])
+        if len(contents) == 0:
+            return float(default)
+
+        omin = cmp(contents)
+        pmin = cmp(contents)
+        while len(contents) > plt.GetNbinsX() * .66:
+            idx = contents.index(pmin)
+            others = []
+            if idx > 0:
+                others.append(contents[idx - 1])
+            if idx < len(contents) - 1:
+                others.append(contents[idx + 1])
+            if op(cmp(others), f * pmin):
+                return pmin
+            contents.remove(pmin)
+            pmin = cmp(contents)
+        return omin
+
     def draw(self, pad=r.gPad):
-        self.__legend.SetNColumns(
-                int(math.ceil(len(self.__plots) / 2.)))
+        # find optimal legend column count: between 4 and 2, optimally
+        # higher and all rows as full as possible
+        mods = {}
+        for i in range(4, 1, -1):
+            m = len(self.__plots) % i
+            if m not in mods:
+                mods[m] = i
+        self.__legend.SetNColumns(mods[min(mods.keys())])
+        height = math.ceil(len(self.__plots) / mods[min(mods.keys())])
+        self.__legend.SetY1(self.__legend.GetY2() - .05 * height)
 
         pad.Divide(1,2)
         pad.GetPad(1).SetPad(0., .333, 1., 1.)
@@ -108,25 +173,69 @@ class Plots:
         r.gPad.SetTopMargin(.05)
         r.gPad.SetTickx(2)
         r.gPad.Modified()
+        self.find_x_limits()
         if self.__logplot:
             r.gPad.SetLogy(True)
 
+        # first rebin, then determine limits
+        drawn_plots = []
         first = True
-        for plt in reversed(self.__plots):
+        for plt in self.__plots:
+            if first:
+                if self.__limits:
+                    xaxis = plt.GetXaxis()
+                    ratio = xaxis.GetNbins() * \
+                            float.__rsub__(*map(float, self.__limits)) / \
+                            (xaxis.GetXmax() - xaxis.GetXmin()) / \
+                            80.
+                else:
+                    ratio = xaxis_up.GetNbins() / 80.
+                ratio = max(1, int(ratio))
+                drawn_plots.append(plt.Rebin(ratio))
+                first = False
+            else:
+                drawn_plots.append(plt.Rebin(ratio))
+
+        # now determine y-axis plotting range
+        ymax, ymin = 0., float("inf")
+        for plt in drawn_plots:
+            ymin = min(self.get_min(plt), ymin)
+            ymax = max(self.get_max(plt), ymax)
+
+        if not self.__logplot:
+            ymin = 0.
+        else:
+            ymax *= 4.
+
+        first = True
+        for plt in reversed(drawn_plots):
+            if "p" in plt.GetOption().lower():
+                # plt.SetOption("")
+                opt = "p"
+            else:
+                # plt.SetOption("")
+                opt = ""
+
             if first:
                 plt.SetTitle(self.__title)
                 plt.SetTitle("")
 
                 xaxis_up = plt.GetXaxis()
                 yaxis_up = plt.GetYaxis()
+                yaxis_up.SetTickLength(.02)
+                yaxis_up.SetRangeUser(ymin * .5, ymax * 1.2)
+
                 if self.__limits:
                     xaxis_up.SetRangeUser(*self.__limits)
-                yaxis_up.SetTickLength(.02)
 
-                plt.DrawCopy()
+                # plt.DrawCopy(plt.GetOption())
+                plt.DrawCopy("hist" + opt)
+                # plt.DrawCopy()
                 first = False
             else:
-                plt.DrawCopy("same")
+                # plt.DrawCopy("same" + plt.GetOption())
+                plt.DrawCopy("same hist" + opt)
+                # plt.DrawCopy("same")
 
         self.__legend.Draw()
 
@@ -135,21 +244,24 @@ class Plots:
         r.gPad.SetTopMargin(1e-5)
         r.gPad.SetTickx(1)
 
-        ref = self.__plots[0].Clone()
-        rel_plts = []
-
-        ymax, ymin = 1.1, 0.9
-
-        for plt in self.__plots[1:]:
+        ref = drawn_plots[0].Clone()
+        ymax, ymin = 0., float("inf")
+        for plt in drawn_plots[1:]:
             copy = plt.Clone()
             copy.Divide(ref)
-            ymin = min(copy.GetMinimum(.0001), ymin)
-            ymax = max(copy.GetMaximum(), ymax)
+            ymin = min(self.get_min(copy), ymin)
+            ymax = max(self.get_max(copy), ymax)
 
-        r.gPad.SetLogy(True)
+        if ymax / ymin > 10.:
+            r.gPad.SetLogy(True)
+            ymax *= 2.
+            ymin *= .7
+        else:
+            ymax *= 1.1
+            ymin *= 0.9
 
         first = True
-        for plt in reversed(self.__plots[1:]):
+        for plt in reversed(drawn_plots[1:]):
             copy = plt.Clone()
             copy.SetTitle(self.__title)
             copy.SetTitle("")
@@ -157,12 +269,12 @@ class Plots:
             if self.__limits:
                 copy.GetXaxis().SetRangeUser(*self.__limits)
 
-            if "p" in copy.GetOption().lower():
-                copy.SetOption("")
-                opt = "hist p"
+            if "p" in plt.GetOption().lower():
+                # copy.SetOption("")
+                opt = "p"
             else:
-                copy.SetOption("")
-                opt = "hist l"
+                # copy.SetOption("")
+                opt = ""
 
             if first:
                 xaxis_down = copy.GetXaxis()
@@ -180,29 +292,21 @@ class Plots:
                 yaxis_down.SetTitleSize(2 * yaxis_up.GetTitleSize())
                 yaxis_down.SetRangeUser(ymin, ymax)
 
-                copy.DrawCopy(opt)
+                copy.DrawCopy("hist" + opt)
+                # copy.DrawCopy()
                 first = False
             else:
-                copy.DrawCopy("same" + opt)
-            rel_plts.append(copy)
+                copy.DrawCopy("same hist" + opt)
+                # copy.DrawCopy("same")
 
         pad.Modified()
         pad.Update()
-
-    def get_legend(self):
-        return self.__legend
-
-    def is_logplot(self):
-        return self.__logplot
 
     def set_logplot(self, log):
         self.__logplot = log
 
     def set_title(self, title):
         self.__title = title
-
-    def get_title(self):
-        return self.__title
 
 def create_stack(hists, files, norms=None, adjustlimits=True, limits=None, logplot=False, normalized=True, 
         title=''):
@@ -270,6 +374,13 @@ def legend(path, hist):
     f, dir = path.split(':', 1)
     basedir = dir.lower().strip('/')
 
+    # XXX ugly hack to correct rechit labelling
+    if 'transverse' not in basedir and 'rechit' in basedir:
+        hist.GetYaxis().SetTitle(
+                hist.GetYaxis().GetTitle().replace("E_{T}", "E"))
+        hist.GetXaxis().SetTitle(
+                hist.GetXaxis().GetTitle().replace("E_{T}", "E"))
+
     m = re.search(r'plots_([a-zA-Z0-9]+)(?:_([^_]+))?_(?:([a-zA-Z0-9+]+)-)(\w+).root$', f)
     if m:
         (label, mod, tier, pu) = m.groups()
@@ -278,21 +389,33 @@ def legend(path, hist):
             if f not in mc_files:
                 mc_files.append(f)
             if isinstance(hist, r.TProfile):
-                hist.SetOption("P E")
+                hist.SetOption("PE")
             hist.SetLineColor(mc_colors[mc_files.index(f)])
             hist.SetMarkerColor(mc_colors[mc_files.index(f)])
-            hist.SetMarkerStyle(25)
-            hist.SetMarkerSize(1)
+            if 'reemul' in basedir:
+                hist.SetMarkerStyle(r.kOpenDiamond)
+            elif 'reweighted' in basedir:
+                hist.SetMarkerStyle(r.kOpenCross)
+            else:
+                hist.SetMarkerStyle(r.kOpenSquare)
+            hist.SetMarkerSize(.8)
         elif label == 'data':
             label = label.capitalize()
             if isinstance(hist, r.TProfile):
-                hist.SetOption("P E")
+                hist.SetOption("PE")
+            # else:
+                # hist.SetOption("P")
             if f not in data_files:
                 data_files.append(f)
             hist.SetLineColor(data_colors[data_files.index(f)])
             hist.SetMarkerColor(data_colors[data_files.index(f)])
-            hist.SetMarkerStyle(8)
-            hist.SetMarkerSize(1)
+            if 'reemul' in basedir:
+                hist.SetMarkerStyle(r.kFullTriangleUp)
+            elif 'reweighted' in basedir:
+                hist.SetMarkerStyle(r.kFullSquare)
+            else:
+                hist.SetMarkerStyle(r.kFullDotLarge)
+            hist.SetMarkerSize(.5)
         else:
             raise
 
@@ -300,61 +423,26 @@ def legend(path, hist):
 
         if mod:
             label += ' {m}'.format(m=modifiers[mod] if mod in modifiers else mod)
-                
     else:
         sys.stderr.write('filename does not match expected pattern: \
                 {f}\n'.format(f=f))
         raise
+
+    if 'reemul' in basedir:
+        basedir = basedir.replace('reemul', '')
+        hist.SetLineStyle(r.kDotted)
+        label += ' reemul.'
+
+    if 'reweighted' in basedir:
+        basedir = basedir.replace('reweighted', '')
+        hist.SetLineStyle(r.kDashed)
+        label += ' rw.'
 
     n = counts[f][0]
     # Hack to get the right normalization for pileup histograms
     if 'pileup' in f:
         hist.Scale(1. / hist.Integral())
         n = 1
-
-    if 'data' in label.lower():
-        color = 1
-        # hist.SetMarkerSize(3)
-        hist.SetMarkerStyle(r.kFullDotMedium)
-    elif 'reweighted' in dir.lower():
-        label += ' rw.'
-        color = 3
-    else:
-        color = 2
-
-    if mod:
-        color += 2
-
-    if mc_cmp:
-        if f not in colors:
-            globals()['last_color'] += 1
-            colors[f] = last_color
-        color = colors[f]
-        try:
-            m = re.search(r'Tune[^_]+', f)
-            if not m:
-                raise
-
-            label = m.group(0)
-
-            if 'reweighted' in dir.lower():
-                label += ' rw.'
-        except:
-            pass
-
-    if 'reemul' in basedir:
-        basedir = basedir.replace('reemul', '')
-        if mc_cmp:
-            color += 10
-        else:
-            if label == 'Data':
-                color = 16
-            else:
-                color *= 3
-        # hist.SetLineStyle(r.kDashed)
-        label += ' reemul.'
-
-    hist.SetLineColor(color)
 
     return label, n, basedir, hist
 
@@ -364,25 +452,25 @@ def get_num_events(fn):
     L1 energy sums or number of vertices for this.
     """
     c, c_rw = (0, 0)
+
+    hist = r.gDirectory.Get('{f}:trackPlotter/vertices'.format(f=fn))
+    hist_rw = r.gDirectory.Get('{f}:reWeightedTrackPlotter/vertices'.format(f=fn))
+    if hist:
+        c = hist.GetEntries()
+    if hist_rw:
+        c_rw = hist.Integral()
+    
+    return (c, c_rw)
     
     hist = r.gDirectory.Get('{f}:gctPlotter/et_tot'.format(f=fn))
     hist_rw = r.gDirectory.Get('{f}:reWeightedGctPlotter/et_tot'.format(f=fn))
     if hist:
-        c = hist.Integral()
+        c = hist.GetEntries()
     if hist_rw:
         c_rw = hist.Integral()
 
     if c != 0:
         return (c, c_rw)
-
-    hist = r.gDirectory.Get('{f}:trackPlotter/vertices'.format(f=fn))
-    hist_rw = r.gDirectory.Get('{f}:reWeightedTrackPlotter/vertices'.format(f=fn))
-    if hist:
-        c = hist.Integral()
-    if hist_rw:
-        c_rw = hist.Integral()
-    
-    return (c, c_rw)
 
 def summarize(pdffile, files):
     handles = [r.TFile(fn) for fn in files]
@@ -399,13 +487,18 @@ def summarize(pdffile, files):
         for d in f.GetListOfKeys():
             dir = d.ReadObj()
             path = dir.GetPath()
+
             for k in dir.GetListOfKeys():
                 key = k.GetName()
                 obj = k.ReadObj()
                 leg, norm, basedir, obj = legend(path, obj)
 
+                if not re.match(plot_only, basedir, re.IGNORECASE):
+                    continue
+
                 if obj.GetEntries() == 0:
                     print "{k} in {p} empty!".format(k=key, p=path)
+                    continue
 
                 if 'trig' in path.lower():
                     # TODO remove hotfix after fixing hist naming in
@@ -541,7 +634,7 @@ def summarize(pdffile, files):
         
         # print subdict.keys()
 
-if len(sys.argv) < 3:
+if len(new_args) < 2:
     sys.stderr.write(
             "usage: {p} output file...\n".format(p=sys.argv[0]))
     sys.exit(1)
