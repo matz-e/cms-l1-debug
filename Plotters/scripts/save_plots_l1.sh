f5eeed3291d4d31447f24861d9f9425e4380da16
Matthias Wolf <matthias@sushinara.net>
2013-05-10 18:01:09 -0500

    Uncomment plots again.

--- Plotters/scripts/save_plots_l1.sh
+++ Plotters/scripts/save_plots_l1.sh
@@ -21,20 +21,20 @@ for d in $dirs; do
    save_plots.py reemulated=n reweighed=n unweighed=y plot_only="$d\$" "cmp/{d}/recoVLPU_{p}.eps" \
       plots_{data,mc}_*reco-none*.root &
 
-   # save_plots.py unweighed=n plot_only="$d\$" "cmp/{d}/recoAll_{p}.eps" \
-      # plots_{data,mc}_*reco-{2012C,45,66}.root &
-   # save_plots.py unweighed=n plot_only="$d\$" "cmp/{d}/recoAllDir_{p}.eps" \
-      # plots_{data,mc}_*reco-{2012C,front,back}.root &
-   # save_plots.py unweighed=n plot_only="$d\$" "cmp/{d}/recoAllDir2012C_{p}.eps" \
-      # plots_{data,mc}_*reco-{2012C,front,back}*.root &
-   # for p in $pus; do
-      # save_plots.py reemulated=n reweighed=y unweighed=y plot_only="$d\$" "cmp/{d}/reco${p}_{p}.eps" \
-         # plots_{data,mc}_*reco-${p}*.root &
-      # save_plots.py reemulated=n reweighed=y unweighed=n plot_only="$d\$" "cmp/{d}.reweigh/reco${p}_{p}.eps" \
-         # plots_{data,mc}_*reco-${p}*.root &
-      # save_plots.py reemulated=y reweighed=y unweighed=n plot_only="$d\$" "cmp/{d}.reemul/reco${p}_{p}.eps" \
-         # plots_{data,mc}_*reco-${p}*.root &
-   # done
+   save_plots.py unweighed=n plot_only="$d\$" "cmp/{d}/recoAll_{p}.eps" \
+      plots_{data,mc}_*reco-{2012C,45,66}.root &
+   save_plots.py unweighed=n plot_only="$d\$" "cmp/{d}/recoAllDir_{p}.eps" \
+      plots_{data,mc}_*reco-{2012C,front,back}.root &
+   save_plots.py unweighed=n plot_only="$d\$" "cmp/{d}/recoAllDir2012C_{p}.eps" \
+      plots_{data,mc}_*reco-{2012C,front,back}*.root &
+   for p in $pus; do
+      save_plots.py reemulated=n reweighed=y unweighed=y plot_only="$d\$" "cmp/{d}/reco${p}_{p}.eps" \
+         plots_{data,mc}_*reco-${p}*.root &
+      save_plots.py reemulated=n reweighed=y unweighed=n plot_only="$d\$" "cmp/{d}.reweigh/reco${p}_{p}.eps" \
+         plots_{data,mc}_*reco-${p}*.root &
+      save_plots.py reemulated=y reweighed=y unweighed=n plot_only="$d\$" "cmp/{d}.reemul/reco${p}_{p}.eps" \
+         plots_{data,mc}_*reco-${p}*.root &
+   done
 done
 
 wait
